5c020855db8c113f33b71d5bb5869c91b6dfb3d3
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2022-11-21 13:40:58 -0600

    transferred files over from other repository

---
+++ js/external.js
@@ -0,0 +1,8 @@
+'use strict'
+console.log('Hello from external Javascript');
+
+alert('Welcome to my website!');
+
+let question = prompt('What is your favorite color?');
+
+let answer = alert(`I like the color ${question} also.`);
