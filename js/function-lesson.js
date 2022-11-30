1790eac463ee15baae12dcc9181cad863c5d8b74
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2022-11-30 14:45:49 -0600

    added loops js/html file

--- js/function-lesson.js
+++ js/function-lesson.js
@@ -25,6 +25,20 @@ function fight(person1, person2){
     console.log(`${person1} punched ${person2} right in the kisser.`);
 }
 
+
+})();
+console.log('Below is the the isEven function')
+console.log(isEven(24))
+console.log('Below is the isOdd function')
+console.log(isOdd(23))
+console.log('Below is the identity function')
+console.log(identity('test'))
+console.log('Below is the isFive function')
+console.log(isFive(5))
+console.log('Below is the isMultipleOfFive')
+console.log(isMultipleOfFive(25))
+
+
 function isEven(num){
     //Get the pieces of the Puzzle!!!
     //Put the pieces of the puzzle into variables.
@@ -33,4 +47,22 @@ function isEven(num){
     //finally return the answer
     return answer;
 }
-})();
+function isOdd(num) {
+    return num % 2 !== 0;
+};
+
+function identity(input) {
+    return input;
+}
+function isFive(num) {
+    return num === 5;
+}
+function addFive(num) {
+    return num + 5;
+}
+function isMultipleOfFive(num) {
+    return num % 5 === 0;
+}
+function isThree(num) {
+    return num === 3;
+}
