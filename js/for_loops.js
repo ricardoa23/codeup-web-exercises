baf133249a32f16f92c5f973b22fa3f40ca83268
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2022-12-05 10:32:03 -0600

    made correction

--- js/for_loops.js
+++ js/for_loops.js
@@ -8,51 +8,23 @@ showMultiplicationTable(9);
 //
 //
 // My code below (didn't add any code to generate number)
-for (let i = 20; i <= 200; i++) {
-    if (i % 2 === 0) {
-        console.log(`${i} is even`);
+
+function randomNumber(max, min) {
+   let num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+}
+for (let i = 0; i < 10; i++) {
+    let number = randomNumber(200, 20);
+    if (number % 2 === 0) {
+        console.log(`${number} is even`);
     } else {
-        console.log(`${i} is odd`);
+        console.log(`${number} is false`);
     }
 }
-//
-//
-//
-// for (let i = 100; i >= 1; i--) {
-//     if (i % 5 === 0) {
-//         console.log(i);
-//     }
-// }
-//instructor's method
 
 
-
-
-// function getRandomNumber() {
-// return Math.Floor(Math.random()) * max - min + 1) + min)
-// }
-//
-// for (let i = 0; i < 10; i++) {
-//     let randomNumber = getRandomNumber();
-// }
-
-
-// for ( let i = 0; i < 9; i++) {
-//     let number = i + 1; // this is the actual number that printed
-//     let result = number.toString(); // Making it a string, so any operators concatenate in the results
-//     for (let j = 1; j <= i; j++) {
-//         result += number; // concatenating the numbers at the end of the result
-//     }
-//     console.log(result)
-// }
-// //Using a Repeat method
-// for ( let i = 0; i < 9; i++) {
-//     let number = String(i + 1).repeat(i + 1)
-//     console.log(number)
-//
-//
-//
-//     for (let i = 100; i >= 5; i -= 5) {
-//     console.log(i)
-// }
-//}
+for (let i = 100; i >= 1; i--) {
+    if (i % 5 === 0) {
+        console.log(i);
+    }
+}
