3c2376ce6855f85badff5f618ba79d1895748761
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2022-12-07 14:24:08 -0600

    Created circle.js and math-js.html

---
+++ js/circle.js
@@ -0,0 +1,41 @@
+(function() {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+
+        getArea: function () {
+            // TODO: complete this method
+            return Math.PI * this.radius ** 2;
+            // TODO: return the proper value
+        },
+
+        // TODO: complete this method
+        // If doRounding is true, round the result to the nearest integer.
+        // Otherwise, output the complete value
+        logInfo: function (doRounding) {
+            if(doRounding) {
+                let areaRounded = Math.round(this.getArea())
+                console.log(`Area of a circle with radius: ${this.radius} is: ${areaRounded} `);
+            } else {
+                console.log(`Area of a circle with radius: ${this.radius} is: ${this.getArea(this.radius)} `);
+            }
+        }
+    };
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+    console.log("=======================================================");
+
+    // TODO: Change the radius of the circle to 5.
+    circle.radius = 5
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+})();
