b23e29171b12d7bc80c882c848ccd485712c1b24
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2022-12-16 11:39:33 -0600

    added flexbox lecture files

--- js/challenges.js
+++ js/challenges.js
@@ -1,6 +1,59 @@
 // ================================= WARM UP
 
 // Create a function, findAverageDogAge, that takes in a array of pet objects with age properties and returns the average age of a dog.
+//
+//     const pets = [
+//     {
+//         name: 'Sparky',
+//         type: 'Fish',
+//         age: 4
+//     },
+//     {
+//         name: 'Mr. Pig',
+//         type: 'Cat',
+//         age: 4
+//     },
+//     {
+//         name: 'Bubba',
+//         type: 'Dog',
+//         age: 5
+//     },
+//     {
+//         name: 'Pickles',
+//         type: 'Dog',
+//         age: 10
+//     }
+//
+// ];
+
+// findAverageDogAge(pets) // returns 7.5
+//
+//
+//
+// function findAverageDogAge(pets) {
+//     let totalAge = 0
+//     let averageAge;
+//     if (pets.type === 'Dog') {
+//         for (let i = 0; i< pets.length; i++) {
+//             pets[i].age = 0
+//             totalAge += pets.age
+//         }
+//         return totalAge
+//     }
+// }
+//
+// console.log(findAverageDogAge(pets))
+
+
+
+
+
+
+
+
+// ================================= WARM UP
+
+// Create a function, returnLongestPetName, that takes in a array of pet objects and returns a string of the longest name for a pet.
 
     const pets = [
     {
@@ -19,27 +72,24 @@
         age: 5
     },
     {
-        name: 'Pickles',
+        name: 'Beans',
         type: 'Dog',
-        age: 10
+        age: 3
+    },
+    {
+        name: 'Mr. Salmon',
+        type: 'Fish',
+        age: 1
     }
-
 ];
 
-findAverageDogAge(pets) // returns 7.5
-
-
-
-function findAverageDogAge(pets) {
-    let totalAge = 0
-    let averageAge;
-    if (pets.type === 'Dog') {
-        for (let i = 0; i< pets.length; i++) {
-            pets[i].age = 0
-            totalAge += pets.age
+console.log(returnLongestPetName(pets)); // returns 'Mr. Salmon'
+function returnLongestPetName(input) {
+    let longestPetName = ''
+    input.forEach( (pet) => {
+        if (pet.name.length > longestPetName.length) {
+            longestPetName = pet.name
         }
-        return totalAge
-    }
+    })
+    return longestPetName;
 }
-
-console.log(findAverageDogAge(pets))
