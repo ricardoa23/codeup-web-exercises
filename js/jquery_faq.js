e080dfb5f7db2eddfa95638506231bfbfaff1271
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2023-01-18 14:37:05 -0600

    completed jquery traversing exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,7 +1,21 @@
-$('button').on('click', function() {
+$('.glacier-button').on('click', function() {
     $('dd').toggleClass('invisible')
 })
 
 $('dt').on('click', function() {
     $(this).toggleClass('highlight')
+})
+
+$('.other-button').on('click', function () {
+    $('ul').each(function() {
+        $(this).children('li').last().css('background-color', 'yellow')
+    })
+})
+
+$('h3').on('click', function() {
+    $(this).next().children('li').css('font-weight', 'bold')
+})
+
+$('li').on('click', function() {
+    $(this).parent().children('li').first().css('color', 'blue')
 })
