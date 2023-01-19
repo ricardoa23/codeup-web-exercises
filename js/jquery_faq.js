4ca0336600c55c61958f750cb3f232f4f3618ece
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2023-01-19 09:02:07 -0600

    completed jquery traversing exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,21 +1,38 @@
-$('.glacier-button').on('click', function() {
-    $('dd').toggleClass('invisible')
-})
+$(".glacier-button").on("click", function () {
+  $("dd").toggleClass("invisible");
+});
 
-$('dt').on('click', function() {
-    $(this).toggleClass('highlight')
-})
+$("dt").on("click", function () {
+  $(this).toggleClass("highlight");
+});
 
-$('.other-button').on('click', function () {
-    $('ul').each(function() {
-        $(this).children('li').last().css('background-color', 'yellow')
-    })
-})
+$(".other-button").on("click", function () {
+  $("ul").each(function () {
+    $(this).children().last().css("background-color", "yellow");
+  });
+});
 
-$('h3').on('click', function() {
-    $(this).next().children('li').css('font-weight', 'bold')
-})
+$("h3").on("click", function () {
+  $(this).next().children("li").css("font-weight", "bold");
+});
 
-$('li').on('click', function() {
-    $(this).parent().children('li').first().css('color', 'blue')
-})
+$("li").on("click", function () {
+  $(this).parent().children().first().css("color", "blue");
+});
+
+$("#left-button").on("click", function () {
+  $("#painting-1").insertAfter($("#painting-2"));
+});
+
+$("#center-button").on("click", function () {
+  let ranNum = Math.round(Math.random() * 10);
+  if (ranNum <= 5) {
+    $("#painting-2").insertBefore($("#painting-1"));
+  } else {
+    $("#painting-2").insertAfter($("#painting-3"));
+  }
+});
+
+$("#right-button").on("click", function () {
+  $("#painting-3").insertBefore($("#painting-2"));
+});
