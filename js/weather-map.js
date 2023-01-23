d32c7d063a9463d62360a6036d4cf73da7f5bc0e
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2023-01-23 16:53:14 -0600

    working weather map project

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,10 +1,35 @@
-    mapboxgl.accessToken = keys.mapbox;
+mapboxgl.accessToken = keys.mapbox;
+let map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/ricardoa11/cld97tp9h000s01mzzfbtye7k",
+  zoom: 10,
+  center: [-98.4916, 29.4252],
+  projection: 'globe'
+});
 
-    const map = new mapboxgl.Map({
-      container: "map", // container ID
-      style: "mapbox://styles/mapbox/streets-v12", // style URL
-      center: [-98.49542639905354, 29.424632538592306], // starting position [lng, lat]
-      zoom: 11, // starting zoom
-    });
+  let marker = new mapboxgl.Marker();
+  
+ let coordinates = {lat:29.4252, lng: -98.4916};
 
-    console.log('hello')
+  function addMarker (event) {
+  coordinates = event.lngLat;
+    marker.setLngLat(coordinates).addTo(map);
+    console.log('Lng:', coordinates.lng, 'Lat:', coordinates.lat);
+    return coordinates;
+  }
+  map.on('click', addMarker);
+
+  console.log(coordinates);
+$.get(`https://api.openweathermap.org/data/2.5/forecast`, {
+    APPID: keys.weatherMap,
+    lat: coordinates.lat,
+    lon: coordinates.lng,
+    units: "imperial"
+}).done(function(data) {
+  for (let i = 0; i < data.list.length; i += 8) {
+    console.log(data)
+  }
+});
+
+
+
