6ad62d451a68645f392e1d908c24bdd846867d8a
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2023-01-24 19:54:39 -0600

    completed weather project

--- js/weather-map.js
+++ js/weather-map.js
@@ -19,65 +19,48 @@
       marker: false,
     })
   );
-  console.log(coordinates);
+
   map.on("moveend", function (e) {
     mapSearchCoord = map.getCenter();
     coordinates.lng = mapSearchCoord.lng;
     coordinates.lat = mapSearchCoord.lat;
     marker.setLngLat(coordinates).addTo(map);
     console.log(coordinates);
-    $.get(`https://api.openweathermap.org/data/2.5/forecast`, {
-      APPID: keys.weatherMap,
-      lat: coordinates.lat,
-      lon: coordinates.lng,
-      units: "imperial",
-    })
-      .done(function (data) {
-        $("#weather-data").empty().append(creatingDataCards(data));
-      })
-      .fail(function (jqXhr, status, error) {
-        console.log(jqXhr);
-        console.log(status);
-        console.log(error);
-      });
+    apiCall(e)
   });
-
-  // add marker
-  map.on("click", addMarker);
-  //display SA weather on load
-  $.get(`https://api.openweathermap.org/data/2.5/forecast`, {
-    APPID: keys.weatherMap,
-    lat: coordinates.lat,
-    lon: coordinates.lng,
-    units: "imperial",
+// Loads weather on load
+  map.on('load', function(e) {
+    apiCall(e)
   })
-    .done(function (data) {
-      $("#weather-data").empty().append(creatingDataCards(data));
-    })
-    .fail(function (jqXhr, status, error) {
-      console.log(jqXhr);
-      console.log(status);
-      console.log(error);
-    });
-  //updates data on click
-  map.on("click", apiCall);
-  // creates data on click
+  // add marker
+  map.on("click", function(e) {
+    addMarker(e);
+    apiCall(e);
+  });
+ 
   function creatingDataCards(data) {
     let html = "";
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
     for (let i = 0; i < data.list.length; i += 8) {
       let date = new Date(data.list[i].dt * 1000);
-      date = date.toLocaleDateString("en-US");
-      html += `<div class="weather-cards">
-    <div class="date">${date}</div>
-    <div ><img src="http://openweathermap.org/img/w/${
-      data.list[i].weather[0].icon
-    }.png" /></div>
-    <h4>Weather Description: ${data.list[i].weather[0].description}</h4>
-    <p>High: ${parseInt(data.list[i].main.temp_max)}&#8457</p>
-    <p>Low: ${parseInt(data.list[i].main.temp_min)}&#8457</p>
-    <p>Feels Like: ${parseInt(data.list[i].main.feels_like)}&#8457</p>
-    <p>Pressure: ${parseInt(data.list[i].main.pressure)} hPa</p>
-    </div>`;
+      date = date.toLocaleDateString("en-US", options);
+      html += `<div class="card bg-secondary-subtle m-3">
+    <div class="card-header bg-secondary fs-4 text-center">${date}</div>
+    <div class="d-flex justify-content-center">
+    <img id="wicon"src="http://openweathermap.org/img/w/${
+      data.list[i].weather[0].icon}.png" />
+      </div>
+    <p class="text-center text-uppercase fs-5 fw-bold">${data.list[i].weather[0].description}</p>
+    <p class="text-center my-1">High ${parseInt(data.list[i].main.temp_max)}&#8457</p>
+    <p class="text-center my-1">Low ${parseInt(data.list[i].main.temp_min)}&#8457</p>
+    <p class="text-center my-1">Feels Like ${parseInt(data.list[i].main.feels_like)}&#8457</p>
+    <p class="text-center my-1">Pressure ${parseInt(data.list[i].main.pressure)} hPa</p>
+    </div class="card-footer">`;
       console.log(data);
     }
     return html;
@@ -109,15 +92,3 @@
 
   console.log(coordinates);
 })();
-
-// function searchUpdate(data, userInput) {
-//   data = data.city.name;
-//   data = data.tolowerCase();
-//   userInput = document.querySelector('#search-bar').value;
-//   userInput = userInput.tolowerCase();
-//   if (data === userInput) {
-//     coordinates.lat = data.city.coord.lat;
-//     coordinates.lng = data.city.coord.lon;
-//     map.flyTo()
-//   }
-// }
